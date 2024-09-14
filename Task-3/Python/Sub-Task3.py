38308d6d3e4567834430860b38ca930afd8e016c
Surya-132 <krishna.surya.reddy.2k6@gmail.com>
2024-09-14 16:45:35 +0530

    Delete Task-3 directory

--- Task-3/Python/Sub-Task3.py
+++
@@ -1,7 +0,0 @@
-def pyramid(rows):
-    for i in range(0,rows,2):
-        print(" " * (rows - i - 1) + "* " * (i + 1))
-    for j in range(rows - 2, 0, -2):
-        print(" " * (rows - j) + "* " * j)
-rows = int(input("Enter the number of rows: "))
-pyramid(rows)
